37966c7376773e820f7f0925c660c335f0fe2e7c
Savelyev Ivan <savelyev.exceedteam@gmail>
2020-12-21 09:11:19 +0300

    todo list final

--- index.js
+++ index.js
@@ -1,38 +1,63 @@
+
 const taskList = document.querySelector('.task-list');
 const button = document.querySelector('.task__button');
 const taskInput = document.querySelector('.task__input');
 const taskListTemplate = document.querySelector('#task-list-templeate').content;
+const colorPickerItem = document.querySelectorAll('.color-picker__item');
 
 function createNewElement() {
     let taskListElement = taskListTemplate.cloneNode(true);
-    taskListElement.querySelector('.task-list__text').textContent = taskInput.value;
-    // taskListElement.querySelector('.task-list__button').onclick = taskListElementClickHandler
+    let taskListText = taskListElement.querySelector('.task-list__text');
+    taskListText.textContent = taskInput.value;
+    let taskListItem = taskListElement.querySelector('.task-list__item');
+
+    let selectedColor = document.querySelector(".color-picker__item.active");
+    if (selectedColor) {
+      taskListItem.style.background = selectedColor.style.backgroundColor;
+    }
+    else {
+      taskListItem.style.background = randColor(taskListItem);
+    }
     return taskListElement
 }
 
-function randColor(element) {
+colorPickerJs.addEventListener("click", function(evt){
+  colorPickerItem.forEach(item => {
+    if (item == evt.target) {
+      evt.target.classList.toggle("active");
+    }
+    else {
+      item.classList.remove("active");
+    }
+  })
+})
+
+function randColor() {
     const r = Math.floor(Math.random() * (256));
     const g = Math.floor(Math.random() * (256));
     const b = Math.floor(Math.random() * (256));
     const color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
     
-    element.style.background = color;
+    return color;
 }
 
 function buttonClickHandler () {
-    taskList.appendChild(createNewElement())
-    randColor(taskList.lastElementChild); 
-    taskInput.value = "";
+    if (taskInput.value === "") {
+      alert('Заполните Поле!');
+    } else {
+      taskList.appendChild(createNewElement()) 
+      taskInput.value = "";
+    }
 }
 
 button.addEventListener('click', buttonClickHandler);
 
 
-function taskListElemntClickHandler(event) {
-    let li = event.target.closest('.task-list__item');
-    let button = event.target.tagName === "BUTTON";
+function taskListElemntClickHandler(evt) {
+    let li = evt.target.closest('.task-list__item');
+    let button = evt.target.tagName === "BUTTON";
     if (button) {
-        let close = event.target.parentNode;
+        let close = evt.target.parentNode;
         close.remove();
         
     } else if (li) {
@@ -41,16 +66,3 @@ function taskListElemntClickHandler(event) {
 }
 
 taskList.addEventListener('click', taskListElemntClickHandler);
-
-// function createNewElement() {
-//     // 1. Находим шаблон и копируем содержимое в taskList.
-//     let taskListTemplate = document.querySelector('#task-list-templeate').content;
-//     let taskListElement = taskListTemplate.cloneNode(true);
-//     taskListElement.querySelector('.task-list__text').textContent = taskInput.value;
-//     taskList.appendChild(taskListElement);
-//     taskList.lastElementChild;
-//     randColor(taskList.lastElementChild); 
-
-//     const color = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
-//     return taskListElement
-// }
