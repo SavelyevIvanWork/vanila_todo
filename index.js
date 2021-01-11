00af71c6fc715881610feba022ed18d713e62e6a
Savelyev Ivan <savelyev.exceedteam@gmail>
2021-01-11 10:55:27 +0300

    Классы

--- index.js
+++ index.js
@@ -1,113 +1,140 @@
-const taskList = document.querySelector('.task-list');
-const button = document.querySelector('.task__button');
-const taskInput = document.querySelector('.task__input');
-const taskListTemplate = document.querySelector('#task-list-templeate').content;
-const colorPicker = document.querySelector('#colorPicker');
-const colorPickerItem = document.querySelectorAll('.color-picker__item');
-
-// рандомный цвет нового элемента
-function randColor() {
-  const r = Math.floor(Math.random() * (256));
-  const g = Math.floor(Math.random() * (256));
-  const b = Math.floor(Math.random() * (256));
-  const color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
-  
-  return color;
-}
-
-function color() {
-  let colorPicker = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
-  let color = Math.floor(Math.random() * colorPicker.length);
-}
-
-
-
-// отслеживаем клик колорпикеру и добавляем рамочки целевому элементу 
-colorPicker.addEventListener("click", function(evt) {
-  colorPickerItem.forEach(item => {
-    if (item == evt.target) {
-      evt.target.classList.toggle("color-picker__item--active");
+class TodoModel {
+    constructor() {
+        this._items = [];
+        this.loadToLocalStorage();
     }
-    else {
-      item.classList.remove("color-picker__item--active");
+
+    getItems() {
+        return this._items;
     }
-  })
-})
 
-let newElementArr = []
+    addItem(obj) {
+        this._items.push(obj);
+        this.saveToLocalStorage();
+    }
 
-if (localStorage.getItem('arr')) {
-  newElementArr = JSON.parse(localStorage.getItem('arr'));
+    loadToLocalStorage() {
+        if (localStorage['todo']) {
+            this._items = JSON.parse(localStorage.getItem('todo'));
+        }
+    }
 
-  newElementArr.forEach( function(item, i) {
-    createNewElement(item.todo, i, item.color, item.checked)
-  });
+    completeTask(index){
+        this._items[index].checked = !this._items[index].checked;
+        this.saveToLocalStorage();
+    }
+
+    saveToLocalStorage() {
+        localStorage.setItem('todo', this.getJSON());
+    }
+
+    getJSON() {
+        return JSON.stringify([...this._items]);
+    }
 }
 
-// Добавляем новый элемент на страницу по клике на "Добавить"
-function buttonClickHandler () {
-  const selectedColor = document.querySelector(".color-picker__item--active");
-  const newTodo = {
-    todo: taskInput.value,
-    color: selectedColor ? selectedColor.style.backgroundColor : randColor(),
-    checked: false,
-  };
+class TodoView {
+    createTodo (newTodo) {
+        const taskListTemplate = document.querySelector('#task-list-templeate').content;
+        const todoList = taskListTemplate.cloneNode(true);
+        const todoListItem = todoList.querySelector('.task-list__item');
+        const todoListCheckbox = todoList.querySelector('.task-list__checkbox');
+        const todoListWrapper = todoList.querySelector('.task-list__wrapper');
+        const todoListText = todoList.querySelector('.task-list__text');
 
-    function isValidInput(value) {
-      var reg = /^\s*$/;
-      return reg.test(value) === true
-  }
-     
-  if (isValidInput(taskInput.value)) {
-    alert('Заполните Поле!');
-  } else {
-    createNewElement(taskInput.value, newElementArr.length, newTodo.color, newTodo.checked)
-    newElementArr.push(newTodo);
-    localStorage.setItem('arr', JSON.stringify(newElementArr)) 
-    taskInput.value = "";
-  } 
-}
-
-// создание нового элемента
-function createNewElement(text, id, color, checked) {
-  const todoList = taskListTemplate.cloneNode(true);
-  const todoListItem = todoList.querySelector('.task-list__item');
-  const todoListCheckbox = todoList.querySelector('.task-list__checkbox');
-  const todoListWrapper = todoList.querySelector('.task-list__wrapper');
-  const todoListText = todoList.querySelector('.task-list__text');
-  // const todoListBtn = todoList.querySelector('.task-list__button');
-  todoListCheckbox.style.background = color
-  todoListWrapper.style.background = color
-  todoListText.textContent = text
-  // todoListBtn.id = `btn_${id}`
-  todoListItem.id = `item_${id}`
-  if (checked) {
-    todoListItem.classList.toggle('task-list__item--done')
-  }
-  // taskListItem.onClick = changeStyle(taskListItem,)
-  taskList.appendChild(todoList)
-}
-button.addEventListener('click', buttonClickHandler);
-
-
-taskList.addEventListener('click', function(evt) {
-    const elementId = evt.target.id;
-  
-    newElementArr.forEach(function(item, i) {
-      
-      if (elementId === `btn_${i}`) {
-        taskList.querySelector(`[id='item_${i}']`).classList.add('visually-hidden')
-        newElementArr.splice(i, 1) 
-        localStorage.setItem('arr', JSON.stringify(newElementArr))
+        todoListCheckbox.style.background = newTodo.color
+        todoListWrapper.style.background = newTodo.color
+        todoListText.textContent = newTodo.value
         
-      }
-      if (evt.target.closest(`[id='item_${i}']`)) {
-        item.checked = !item.checked
-        taskList.querySelector(`[id='item_${i}']`).classList.toggle('task-list__item--done')
-        localStorage.setItem('arr', JSON.stringify(newElementArr))
-      }
-      
-    });
-  });
+        todoListItem.id = `item_${newTodo.id}`
+        if (newTodo.checked) {
+          todoListItem.classList.toggle('task-list__item--done')
+        }
+     
+        document.querySelector('.task-list').appendChild(todoList)
+    }
 
-  
+}
+
+class TodoController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+        this.todoList = document.querySelector('.task-list');
+        this.button = document.querySelector('.task__button');
+        this.colorPicker = document.querySelector('#colorPicker');
+        this.colorPickerItem = document.querySelectorAll('.color-picker__item');
+        this.todoInput = document.querySelector('.task__input');
+
+        this.loadTodo();
+        
+        this.todoList.addEventListener('click', this.todoLIstHandler);
+        this.button.addEventListener('click', this.buttonClickHandler);
+        this.colorPicker.addEventListener('click', this.colorPickerHandler);
+    }
+
+    validation = (value) => {
+        var reg = /^\s*$/;
+        return reg.test(value) === true
+    }
+
+    loadTodo = () => {
+        console.log(this.model.getItems())
+        this.model.getItems().forEach((item) => {
+            this.view.createTodo(item)
+        });
+    }
+
+    buttonClickHandler = () => {
+        const selectedColor = document.querySelector(".color-picker__item--active");
+        const newTodo = {
+            value: this.todoInput.value,
+            checked: false,
+            id: model.getItems().length + 1,
+            color: selectedColor ? selectedColor.style.backgroundColor : this.randColor(),
+        }
+           
+        if (this.validation(this.todoInput.value)) {
+          alert('Заполните Поле!');
+        } else {
+            this.view.createTodo(newTodo);
+            this.model.addItem(newTodo);
+            this.model.saveToLocalStorage(); 
+            this.todoInput.value = "";
+        }
+        
+    }
+
+    randColor = () => {
+        let color = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
+        let index = Math.floor(Math.random() * color.length);
+        return color[index]
+    }
+
+    colorPickerHandler = (evt) => {
+        this.colorPickerItem.forEach(item => {
+            if (item == evt.target) {
+              evt.target.classList.toggle("color-picker__item--active");
+            }
+            else {
+              item.classList.remove("color-picker__item--active");
+            }
+        })
+    }
+
+    todoLIstHandler = (evt) => {
+        model.getItems().forEach((item, i) => {
+            if (evt.target.closest(`[id='item_${item.id}']`)) {
+                item.checked = item.checked
+                this.todoList.querySelector(`[id='item_${item.id}']`).classList.toggle('task-list__item--done')
+                this.model.completeTask(i)
+                this.model.saveToLocalStorage()
+            }   
+        });
+    }
+
+}
+
+let model = new TodoModel();
+let view = new TodoView();
+new TodoController(model, view);
--- index2.js
+++ index2.js
@@ -1,213 +1,113 @@
-class TodoModel {
-    constructor() {
-        this._items = [];
-        this.loadToLocalStorage();
-    }
+const taskList = document.querySelector('.task-list');
+const button = document.querySelector('.task__button');
+const taskInput = document.querySelector('.task__input');
+const taskListTemplate = document.querySelector('#task-list-templeate').content;
+const colorPicker = document.querySelector('#colorPicker');
+const colorPickerItem = document.querySelectorAll('.color-picker__item');
 
-    getItems() {
-        return this._items;
-    }
-
-    addItem(obj) {
-        this._items.push(obj);
-        this.saveToLocalStorage();
-    }
-
-    loadToLocalStorage() {
-        if (localStorage['todo']) {
-            this._items = JSON.parse(localStorage.getItem('todo'));
-        }
-    }
-
-    saveToLocalStorage() {
-        localStorage.setItem('todo', this.getJSON());
-    }
-
-    getJSON() {
-        return JSON.stringify([...this._items]);
-    }
-
-    deleteItem(index) {
-        this._items.delete(String(index));
-        this.saveToLocalStorage();
-    }
-}
-
-class TodoView {
-    createTodo (newTodo) {
-        const taskListTemplate = document.querySelector('#task-list-templeate').content;
-        const todoList = taskListTemplate.cloneNode(true);
-        const todoListItem = todoList.querySelector('.task-list__item');
-        const todoListCheckbox = todoList.querySelector('.task-list__checkbox');
-        const todoListWrapper = todoList.querySelector('.task-list__wrapper');
-        const todoListText = todoList.querySelector('.task-list__text');
-
-        todoListCheckbox.style.background = newTodo.color
-        todoListWrapper.style.background = newTodo.color
-        todoListText.textContent = newTodo.value
-        
-        todoListItem.id = `item_${newTodo.id}`
-        if (newTodo.checked) {
-          todoListItem.classList.toggle('task-list__item--done')
-        }
-     
-        document.querySelector('.task-list').appendChild(todoList)
-    }
-
-}
-
-class TodoController {
-    constructor(model, view) {
-        this.model = model;
-        this.view = view;
-        this.todoList = document.querySelector('.task-list');
-        this.button = document.querySelector('.task__button');
-        this.colorPicker = document.querySelector('#colorPicker');
-        this.colorPickerItem = document.querySelectorAll('.color-picker__item');
-        this.todoInput = document.querySelector('.task__input');
-
-        this.loadTodo();
-
-        this.newTodo = {
-            value: this.todoInput.value,
-            checked: false,
-            id: model.getItems().length + 1,
-            color: this.randColor(),
-        }
-        
-        this.todoList.addEventListener('click', this.todoLIstHandler);
-        this.button.addEventListener('click', this.buttonClickHandler);
-    }
-
-    validation = (value) => {
-        var reg = /^\s*$/;
-        return reg.test(value) === true
-    }
-
-    loadTodo = () => {
-        console.log(this.model.getItems())
-        this.model.getItems().forEach((item) => {
-            this.view.createTodo(item)
-        });
-    }
-
-    buttonClickHandler = () => {
-
-        
-        console.log(this.newTodo)
-        
-        this.view.createTodo(this.newTodo);
-        this.model.addItem(this.newTodo);
-        this.model.saveToLocalStorage() 
-        this.todoInput.value = "";
-    }
-
-    randColor = () => {
-        let color = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
-        let index = Math.floor(Math.random() * color.length);
-        return color[index]
-      }
-
-    todoLIstHandler = (evt) => {
-        const elementId = evt.target.id;
-          
-        model.getItems().forEach((item, i) => {
-              
-            if (elementId === `btn_${item.id}`) {
-                this.todoList.querySelector(`[id='item_${item.id}']`).classList.add('visually-hidden')
-                this.model.getItems().splice(i, 1) 
-                this.model.addItem(this.newTodo)
-                this.model.saveToLocalStorage()
-                
-            }
-            if (evt.target.closest(`[id='item_${item.id}']`)) {
-                item.checked = item.checked
-                this.todoList.querySelector(`[id='item_${item.id}']`).classList.toggle('task-list__item--done')
-                this.model.addItem(this.newTodo)
-                this.model.saveToLocalStorage()
-            }
-              
-        });
-    }
-
-}
-
-// function randColor() {
-//     let color = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
-//     let index = Math.floor(Math.random() * color.length);
-//     return color[index];
-//   }
-//   console.log(randColor())
-
-let model = new TodoModel();
-let view = new TodoView();
-new TodoController(model, view);
-
-
-
-// if (localStorage.getItem('todo')) {
-//     this.items = JSON.parse(localStorage.getItem('todo'));
+// рандомный цвет нового элемента
+function randColor() {
+  const r = Math.floor(Math.random() * (256));
+  const g = Math.floor(Math.random() * (256));
+  const b = Math.floor(Math.random() * (256));
+  const color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
   
-//     this.items.forEach( function(item, i) {
-//       createNewElement(item.value, item.checked, item.id, item.color)
-//     });
-// }
+  return color;
+}
+
+function color() {
+  let colorPicker = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
+  let color = Math.floor(Math.random() * colorPicker.length);
+}
 
 
 
+// отслеживаем клик колорпикеру и добавляем рамочки целевому элементу 
+colorPicker.addEventListener("click", function(evt) {
+  colorPickerItem.forEach(item => {
+    if (item == evt.target) {
+      evt.target.classList.toggle("color-picker__item--active");
+    }
+    else {
+      item.classList.remove("color-picker__item--active");
+    }
+  })
+})
+
+let newElementArr = []
+
+if (localStorage.getItem('arr')) {
+  newElementArr = JSON.parse(localStorage.getItem('arr'));
+
+  newElementArr.forEach( function(item, i) {
+    createNewElement(item.todo, i, item.color, item.checked)
+  });
+}
+
+// Добавляем новый элемент на страницу по клике на "Добавить"
+function buttonClickHandler () {
+  const selectedColor = document.querySelector(".color-picker__item--active");
+  const newTodo = {
+    todo: taskInput.value,
+    color: selectedColor ? selectedColor.style.backgroundColor : randColor(),
+    checked: false,
+  };
+
+    function isValidInput(value) {
+      var reg = /^\s*$/;
+      return reg.test(value) === true
+  }
+     
+  if (isValidInput(taskInput.value)) {
+    alert('Заполните Поле!');
+  } else {
+    createNewElement(taskInput.value, newElementArr.length, newTodo.color, newTodo.checked)
+    newElementArr.push(newTodo);
+    localStorage.setItem('arr', JSON.stringify(newElementArr)) 
+    taskInput.value = "";
+  } 
+}
+
+// создание нового элемента
+function createNewElement(text, id, color, checked) {
+  const todoList = taskListTemplate.cloneNode(true);
+  const todoListItem = todoList.querySelector('.task-list__item');
+  const todoListCheckbox = todoList.querySelector('.task-list__checkbox');
+  const todoListWrapper = todoList.querySelector('.task-list__wrapper');
+  const todoListText = todoList.querySelector('.task-list__text');
+  // const todoListBtn = todoList.querySelector('.task-list__button');
+  todoListCheckbox.style.background = color
+  todoListWrapper.style.background = color
+  todoListText.textContent = text
+  // todoListBtn.id = `btn_${id}`
+  todoListItem.id = `item_${id}`
+  if (checked) {
+    todoListItem.classList.toggle('task-list__item--done')
+  }
+  // taskListItem.onClick = changeStyle(taskListItem,)
+  taskList.appendChild(todoList)
+}
+button.addEventListener('click', buttonClickHandler);
 
 
-// const taskList = document.querySelector('.task-list');
-// const button = document.querySelector('.task__button');
-// const todoInput = document.querySelector('.task__input');
-// const todoListTemplate = document.querySelector('#task-list-templeate').content;
-// const colorPicker = document.querySelector('#colorPicker');
-// const colorPickerItem = document.querySelectorAll('.color-picker__item');
-
-
-// class Model {
-//     constructor(todoList) {
-//         this._items = [];
-//         this.loadToLocalStorage();
-//     }
-
-//     changeChecked(index) {
+taskList.addEventListener('click', function(evt) {
+    const elementId = evt.target.id;
+  
+    newElementArr.forEach(function(item, i) {
+      
+      if (elementId === `btn_${i}`) {
+        taskList.querySelector(`[id='item_${i}']`).classList.add('visually-hidden')
+        newElementArr.splice(i, 1) 
+        localStorage.setItem('arr', JSON.stringify(newElementArr))
         
-//     }
+      }
+      if (evt.target.closest(`[id='item_${i}']`)) {
+        item.checked = !item.checked
+        taskList.querySelector(`[id='item_${i}']`).classList.toggle('task-list__item--done')
+        localStorage.setItem('arr', JSON.stringify(newElementArr))
+      }
+      
+    });
+  });
 
-//     loadToLocalStorage() {
-//         window.localStorage['todo'] ? this._items = JSON.parse(window.localStorage['todo']) :  null; 
-        
-//     }
-
-//     saveToLocalStorage() {
-//         window.localStorage.setItem('todo', this.getJSON());
-//     }
-
-//     getJSON() {
-//         return JSON.stringify([...this._items]);
-//     }
-
-//     addTask() {
-
-//     }
-//   }
-
-// // class View {
-// //     constructor() {
-// //         this.todo = [];
-// //     }
-// // }
-
-// class Controller {
-
-//     createElement()
-
-//     changeElementState(element)
-
-//     deleteElement(element)
-
-
-
-// }
-
+  
